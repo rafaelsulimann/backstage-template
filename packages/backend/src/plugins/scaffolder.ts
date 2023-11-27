41cc7c4dc07a5d1e8277de64e82ec81dc715f793
Rafael Xavier Sulimann <rafael.sulimann@totvs.com.br>
2023-11-26 22:42:12 -0300

    Criar action para inserir repositório no github

--- packages/backend/src/plugins/scaffolder.ts
+++ packages/backend/src/plugins/scaffolder.ts
@@ -1,7 +1,9 @@
 import { CatalogClient } from '@backstage/catalog-client';
-import { createRouter } from '@backstage/plugin-scaffolder-backend';
+import { createBuiltinActions, createRouter } from '@backstage/plugin-scaffolder-backend';
 import { Router } from 'express';
 import type { PluginEnvironment } from '../types';
+import { createGithubRepository } from './scaffolder/actions/createGitHubRepository'
+import { ScmIntegrations } from '@backstage/integration';
 
 export default async function createPlugin(
   env: PluginEnvironment,
@@ -9,8 +11,21 @@ export default async function createPlugin(
   const catalogClient = new CatalogClient({
     discoveryApi: env.discovery,
   });
+  const integrations = ScmIntegrations.fromConfig(env.config);
+
+  const builtInActions = createBuiltinActions({
+    integrations,
+    catalogClient,
+    config: env.config,
+    reader: env.reader,
+  });
+
+  const actions = [...builtInActions, 
+    createGithubRepository()
+  ];
 
   return await createRouter({
+    actions,
     logger: env.logger,
     config: env.config,
     database: env.database,
---
+++ packages/backend/src/plugins/scaffolder/actions/createGitHubRepository.ts
@@ -0,0 +1,64 @@
+import { createTemplateAction } from '@backstage/plugin-scaffolder-backend';
+
+export const createGithubRepository = () => {
+  return createTemplateAction<{
+    repositoryName: string;
+    repositoryUserToken: string;
+  }>({
+    id: 'github:create:repo',
+    schema: {
+      input: {
+        required: ['repositoryName', 'repositoryUserToken'],
+        type: 'object',
+        properties: {
+          repositoryName: {
+            type: 'string',
+            title: 'Repository ID',
+            description: 'The ID of the repository.',
+          },
+          repositoryUserToken: {
+            type: 'string',
+            title: 'Repository User Token',
+            description: 'The user token',
+          },
+        },
+      },
+    },
+
+    async handler(ctx) {
+      const { repositoryName, repositoryUserToken } = ctx.input;
+
+      ctx.logger.info(`Repository Name ${repositoryName}`);
+      ctx.logger.info(`Repository UserToken ${repositoryUserToken}`);
+
+      const response = await fetch(`https://api.github.com/user/repos`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+          Authorization: `Bearer ${repositoryUserToken}`,
+        },
+        body: JSON.stringify({
+          name: repositoryName,
+          description: 'testezera',
+          homepage: 'https://github.com',
+          private: false,
+          has_issues: true,
+          has_projects: true,
+          has_wiki: true,
+        }),
+      });
+
+      const data = await response.json();
+
+      if (response.status !== 200) {
+        ctx.logger.error(`Erro ao criar repositório ${data}`);
+        ctx.output('data', data);
+        return response.json();
+      }
+      ctx.logger.info(`Repositório ${repositoryName} criado com sucesso!`);
+      ctx.output('data', data);
+      return response.json();
+    },
+  });
+};
